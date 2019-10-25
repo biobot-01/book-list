46d1be70bb167efa9b313f6a88264380227ee111
Kevin Michael <biobot.mike@gmail.com>
2019-10-25 17:28:32 +0200

    Add book class constructor & UI methods

--- app-es6.js
+++ app-es6.js
@@ -1,5 +1,35 @@
 class Book {
-
+  constructor(title, author, isbn) {
+    this.title = title;
+    this.author = author;
+    this.isbn = isbn;
+  }
 }
 
-clas UI {}
+class UI {
+  addBookToList(book) {
+    const list = document.getElementById('bookList');
+    // Create tr element
+    const tableRow = document.createElement('tr');
+    // Insert data into table
+    tableRow.innerHTML = `
+      <td>${book.title}</td>
+      <td>${book.author}</td>
+      <td>${book.isbn}</td>
+      <td><button class="delete" type="button"></td>`;
+    
+    list.appendChild(tableRow);
+  }
+
+  showAlert(message, className) {
+
+  }
+
+  deleteBook(target) {
+
+  }
+
+  clearFields() {
+
+  }
+}
