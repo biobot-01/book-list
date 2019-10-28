369d907eddb781b660196f4dbdf2222751295281
Kevin Michael <biobot.mike@gmail.com>
2019-10-28 08:45:21 +0200

    Add UI class functions logic & event listeners for buttons

--- app-es6.js
+++ app-es6.js
@@ -22,14 +22,72 @@ class UI {
   }
 
   showAlert(message, className) {
-
+    // Create div
+    const div = document.createElement('div');
+    // Add classes
+    div.className = `notification ${className}`;
+    // Add text
+    div.appendChild(document.createTextNode(message));
+    // Get parent
+    const column = document.querySelector('.column');
+    // Get form
+    const form = document.getElementById('bookForm');
+    // Insert alert
+    column.insertBefore(div, form);
+    // Timeout after 3 secs
+    setTimeout(function() {
+      document.querySelector('.notification').remove();
+    }, 3000);
   }
 
   deleteBook(target) {
-
+    if (target.className === 'delete') {
+      target.parentElement.parentElement.remove();
+    }
   }
 
   clearFields() {
-
+    document.getElementById('title').value = '';
+    document.getElementById('author').value = '';
+    document.getElementById('isbn').value = '';
   }
 }
+
+// Event listener for add book
+document.getElementById('bookForm').addEventListener('submit', function(e) {
+  // Get form values
+  const title = document.getElementById('title').value;
+  const author = document.getElementById('author').value;
+  const isbn = document.getElementById('isbn').value;
+
+  // Instantiate book
+  const book = new Book(title, author, isbn);
+
+  // Instantiate ui
+  const ui = new UI();
+  // Validate form inputs
+  if (title === '' || author === '' || isbn === '') {
+    // Error alert
+    ui.showAlert('Please fill in all fields', 'is-danger');
+  } else {
+    // Add book to list
+    ui.addBookToList(book);
+    // Success alert
+    ui.showAlert('Book Added!', 'is-success');
+    // Clear form fields
+    ui.clearFields();
+  }
+
+  e.preventDefault();
+});
+
+// Event listener for delete book
+document.getElementById('bookList').addEventListener('click', function(e) {
+  // Instantiate ui
+  const ui = new UI();
+  // Delete book from ui
+  ui.deleteBook(e.target);
+  // Show alert
+  ui.showAlert('Book Removed!', 'is-warning');
+  e.preventDefault();
+});
